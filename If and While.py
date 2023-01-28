351ebdae724d6c77991545e5bb19c7f9f99b75f4
Typo-Williams <123336843+Typo-Williams@users.noreply.github.com>
2023-01-27 22:34:26 -0600

    Add files via upload

--- If and While.py
+++ If and While.py
@@ -8,9 +8,9 @@ while last_name != 'ZZZ':
     first_name = str(input("Enter student's first name: "))
     gpa = float(input("Enter student GPA: "))
 
-    if gpa > 3.5:
-        print(first_name, last_name, "is on the Dean's List")
-    elif gpa < 3.5 and gpa > 3.25:
+    if gpa >= 3.5:
+        print(first_name, last_name, "is on the Honor Roll and Dean's List")
+    elif gpa >= 3.25:
         print(first_name, last_name, "is on the Honor Roll")
     else: 
         print(first_name, last_name, "is not on the Honor Roll or Dean's List")
